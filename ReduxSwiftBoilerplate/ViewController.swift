7cefbc535a2a6aa39796e0f007a201ae0013cf4d
Michael Henry Pantaleon <me@iamkel.net>
2017-10-24 21:50:46 +0900

    Initial Commit

---
+++ ReduxSwiftBoilerplate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReduxSwiftBoilerplate
+//
+//  Created by Michael Henry Pantaleon on 2017/10/24.
+//  Copyright © 2017 Michael Henry Pantaleon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
